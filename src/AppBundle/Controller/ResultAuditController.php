e02946fcbf92aa782d3d56f4b63fdbb1b05a4f34
GillesCrebassa <crebassa.gilles@gmail.com>
2016-07-07 16:42:40 +0200

    add ssh component

--- src/AppBundle/Controller/ResultAuditController.php
+++ src/AppBundle/Controller/ResultAuditController.php
@@ -8,6 +8,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Process\Process;
+use \Symfony\Component\Process\Exception\ProcessFailedException;
+
+
+use Ssh;
+use Ssh\SshConfigFileConfiguration;
+use Ssh\Session;
 
 use AppBundle\Entity\Product;
 use AppBundle\Entity\ProductParameter;
@@ -32,20 +38,52 @@ class ResultAuditController extends Controller {
         $product = $repository->find('1');
         $repository = $this->getDoctrine()->getRepository('AppBundle:Environment');
         $environment = $repository->find('1');
+        $root_dir = $this->container->getParameter('kernel.root_dir');
+        $ssh_dir = $root_dir.'/../ssh';
+        
+        
+        
+/*        
+        if (!file_exists($ssh_dir) && !is_dir($ssh_dir)) {
+            mkdir($ssh_dir);
+            chmod($ssh_dir, 0700);
+            $command = '/usr/bin/ssh-keygen -t rsa -C "your_email@example.com" -P "" -f '.$ssh_dir.'/id_rsa';
+            $process = new Process($command);
+            $pid = $process->run();
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+/*            
+            $command = 'ssh-add '.$ssh_dir.'/id_rsa';
+            $process = new Process($command);
+            $pid = $process->run();            
+*/            
 
+/*        
+            $command = 'ssh-copy-id '.$ssh_dir.'/id_rsa.pub pf1@cw1902';
+            $process = new Process($command);
+            $pid = $process->run();
+//            if (!$process->isSuccessful()) {
+//                throw new ProcessFailedException($process);
+//            }
+            
+            
+        }
+/*
         $folder_of_custom_ssh = '/var/www/html/symfony/envMan/.ssh';
         $ip_server = '172.18.50.229';
         $user_environment = 'pf1';
         $command = 'gcc -v';
-        $process = new Process('/usr/bin/ssh -i "'.$folder_of_custom_ssh.'" '.$user_environment.'@'.$ip_server.' "'.$command.'"');
+        $process = new Process('/usr/bin/ssh -i "'.$ssh_dir.'" '.$user_environment.'@'.$ip_server.' "'.$command.'"');
 
         $pid = $process->run();
         //getOutput
         //getErrorOutput
+ */       
         
+        $configuration = new Ssh\SshConfigFileConfiguration('/Users/username/.ssh/config', 'my-host');
         
-        
-        return new Response('<html><body>Hello '.$process->getErrorOutput().'!</body></html>');
+        return new Response('<html><body>Hello '.$process->getErrorOutput().' : root_dir:'.$ssh_dir.': command:'.$command.' !</body></html>');
         
         
         
