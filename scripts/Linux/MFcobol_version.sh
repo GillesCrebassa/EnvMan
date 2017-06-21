dfc3d8e68416d1d53db800b366c643f8f762cdbc
crebassa gilles <GillesCrebassa@users.noreply.github.com>
2017-06-21 13:11:00 +0200

    Delete MFcobol_version.sh

--- scripts/Linux/MFcobol_version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ksh
-. $HOME/.profile 1>/dev/null
-version=$(cob -V 2>/dev/null |grep "version" |cut -f "2")
-wrappack=$(cob -V 2>/dev/null |grep "PTI" |cut -d "=" -f "2")
-echo $version - $wrappack
